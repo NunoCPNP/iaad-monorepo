689ee6b9aeec725be73fd309a62f48082a305c83
Nuno Nunes Pereira <nuno.nunespereira@ffptn3436.farfetch.local>
2021-01-14 17:01:27 +0000

    feat(root): Initial boilerplate

---
+++ common/checkyarn.js
@@ -0,0 +1,13 @@
+if (!/yarn\.js$/.test(process.env.npm_execpath || '')) {
+  console.warn('\u001b[33mThis repository requires Yarn 1.x for scripts to work properly.\u001b[39m\n')
+  process.exit(1)
+}
+
+console.log('')
+console.log(` ___    _        _    ____  `)
+console.log(`|_ _|  / \\      / \\  |  _ \\ `)
+console.log(` | |  / _ \\    / _ \\ | | | |       __        __   __   ___  __   __ `)
+console.log(` | | / ___ \\  / ___ \\| |_| | |\\/| /  \\ |\\ | /  \\ |__) |__  |__) /  \\ `)
+console.log(`|___/_/   \\_\\/_/   \\_\\____/  |  | \\__/ | \\| \\__/ |  \\ |___ |    \\__/ `)
+console.log('') 
+                  
